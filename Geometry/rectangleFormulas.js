bd83700662b4ec83e33152be8a78cc849cadc360
slavajs <dimaidiothack@gmail.com>
2019-06-30 13:48:05 +0300

    Added new figuresgit status!

---
+++ Geometry/rectangleFormulas.js
@@ -0,0 +1,7 @@
+export function findDiagonalFromAB(a, b) {
+    return Math.sqrt(a*a + b*b);
+}
+
+export function findArea(a, b) {
+    return a * b
+}
---
+++ Geometry/squareFormulas.js
@@ -0,0 +1,27 @@
+export function findArea(a) {
+    return a*a
+}
+
+export function findPerimetrFromSide(a) {
+    return 4*a;
+}
+
+export function findDiagonal(a) {
+    return a * Math.sqrt(2);
+}
+
+export function findPerimetrFromCircumcircle(R) {
+    return 4 * R * Math.sqrt(2)
+}
+
+export function findPerimetrFromInscribed(r) {
+    return 8 * r
+}
+
+export function findAreaFromCircumcircle(R) {
+    return 2 * R * R
+}
+
+export function findPerimetrFromInscribed(r) {
+    return 4 * r * r
+}
